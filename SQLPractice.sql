60c4166e326283915efc60ecb7bd190bf9f132cf
kartik <kartikiyer2892@gmail.com>
2023-07-04 14:45:28 +0530

    Added some more SQL answers till A12. Commited through GIT GUI

--- SQLPractice.sql
+++ SQLPractice.sql
@@ -17,18 +17,49 @@ order by LastName
 select productid, productnumber, name from Production.Product
 where SellStartDate is not NULL and productline = 'T'
 order by name
+--A7
+select customerID, sum(Freight) as total_freight
+from sales.SalesOrderHeader
+group by CustomerID
+order by CustomerID
+--A8
+select CustomerID, SalesPersonID, AVG(SubTotal) avg_subTotal , SUM(SubTotal) sum_subTotal
+from Sales.SalesOrderHeader
+group by CustomerID, SalesPersonID
+order by CustomerID desc
+--A9
+select ProductID, SUM(Quantity) Total_Quantity from Production.ProductInventory
+where Shelf = 'A' or Shelf = 'C' or Shelf ='H'
+group by ProductID
+having SUM(Quantity)>500
+order by ProductID
+--A10
+select SUM(Quantity) as total_quantity
+from Production.ProductInventory PI
+group by (LocationID*10)
+--A11
+select P.BusinessEntityID, FirstName, LastName, PhoneNumber from Person.PersonPhone PP
+join Person.Person P
+on PP.BusinessEntityID = P.BusinessEntityID
+where LastName like 'L%'
+order by LastName, FirstName
+--A12
+select SalesPersonID, customerID, SUM(SubTotal) sum_subTotal
+from Sales.SalesOrderHeader
+where SalesPersonID is not null
+group by ROLLUP(SalesPersonID, CustomerID)
+
+
+
 /*A18*/
 select YEAR(OrderDate) yearOfOrder, SUM(totaldue) TotalDueOrder from Sales.SalesOrderHeader
 where YEAR(OrderDate)<=2016
 group by YEAR(OrderDate)
 order by yearOfOrder
 /*A19*/
-
-
-
-
-
-
+select ContactTypeID, Name from Person.ContactType
+where Name like '% Manager'
+order by Name desc
 
 
 
